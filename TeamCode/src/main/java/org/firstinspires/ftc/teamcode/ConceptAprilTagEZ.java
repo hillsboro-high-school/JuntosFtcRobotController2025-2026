c4458fa89633419f5399dcf87312c4d4e132c4bb
volzm975 <volzm975@hsd.k12.or.us>
2025-10-16 17:44:55 -0700

    Works at close range, starts to shake its head at greater distances

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTagEZ.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAprilTagEZ.java
@@ -33,6 +33,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -121,18 +122,27 @@ public class ConceptAprilTagEZ extends LinearOpMode {
                 }
                 */
                 //Code
-                if (SeeAprilTag == false) {
-                    if (CurrentX > 0) {
-                        BSpeed.setPower(-1);
+                telemetry.addData("X", CurrentX);
+                if (SeeAprilTag) {
+
+                    double difference = Math.abs(CurrentX / 200);
+
+                    if (CurrentX > 3){
+                        BSpeed.setPower(0.075 + difference);
+                    }else if (CurrentX < -3){
+                        BSpeed.setPower(-0.075 - difference);
                     }
-                    else {
+                    else{
                         BSpeed.setPower(0);
                     }
-                }
-                //NOTE Make variable for detection
 
-                BSpeed.setPower(0); //-1 - 1
+                }
+                else {
+                    BSpeed.setPower(0);
+                }
+                telemetry.addData("ServoSpeed", BSpeed.getPower());
                 // Share the CPU.
+                telemetry.update();
                 sleep(20);
             }
         }
@@ -169,13 +179,18 @@ public class ConceptAprilTagEZ extends LinearOpMode {
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         telemetry.addData("# AprilTags Detected", currentDetections.size());
 
+        SeeAprilTag = !currentDetections.isEmpty();
+
         // Step through the list of detections and display info for each one.
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
-                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                CurrentX = detection.ftcPose.x;
+                /*telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
                 telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                 telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+
+               telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                 */
             } else {
                 telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
                 telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
